7e6b5dccd14df86e8506512eed69866ff4f57b19
Jo1ceLi <ms0615122@gmail.com>
2021-08-02 23:14:23 +0800

    Test AuthService

--- test/service/auth/auth.service.test.ts
+++ test/service/auth/auth.service.test.ts
@@ -1,82 +1,46 @@
-import { getMongoRepository, createConnection, MongoRepository } from 'typeorm';
-import { permit, authenticate } from '../../../src/service/auth/auth.service';
+import { mock } from 'jest-mock-extended';
+import { AuthService } from '../../../src/service/auth/auth.service';
+import { Container } from 'typeorm-typedi-extensions';
+import { createConnection, useContainer, MongoRepository } from 'typeorm';
 import { createMockContext } from '@shopify/jest-koa-mocks';
-import { userRepo } from '../../../src/repository/user.repository';
 import { User } from '../../../src/Entity/User';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 jest.mock('jsonwebtoken');
 
 beforeAll(async ()=>{
-    const dbEnvs = ['prod', 'default'];
-    let mockUser = { 
-        _id: '33',
-        id: '22',
-        email: 'gg',
-        password: '**'
-    } as User
-    for(let env=0; env < dbEnvs.length; env++)
-    {
-        const conn = await createConnection(dbEnvs[env]);
-        jest.spyOn(userRepo(conn.name), 'findOne').mockResolvedValue(mockUser);
-    }
-
+    useContainer(Container);
+    await createConnection();
     const verify = jwt.verify as jest.MockedFunction<(st: string)=> Record<string, unknown> | string>;
     verify.mockReturnValue({priority: 1, id: 22});
-
 })
 
-describe('Mocking jwt verify and test', () => {
-    const ctx = createMockContext();
-    test('Testing jwt verify match {priority: 1}', ()=>{
-        expect(jwt.verify('', '')).toMatchObject({priority: 1, id: 22});
-    })
-    test('Permit priority 1 to access', () => {
+describe('Auth service ', () => {
+    test('Test jwt verify mocked', async ()=> {
+        let decoded = jwt.verify('', '') as JwtPayload;
+        expect(decoded).toMatchObject({priority: 1, id: 22});
+    });
+    test('Test authLogin next function been called', async ()=> {
+        const mockUserRepo = mock<MongoRepository<User>>();
+        let mockedUser: User = {
+            id: '1',
+            email: "ms",
+            password: ""
+        }
+        mockUserRepo.findOneOrFail.mockResolvedValue(mockedUser);
+        let authService = new AuthService(mockUserRepo);
+        let ctx = createMockContext();
         ctx.request.headers.authorization = '12341234123';
-        permit([1,2,3,4,5])(ctx, async()=>{});
+        let next = jest.fn();
+        await authService.authLogin(ctx,next)
+        expect(next).toBeCalled();
+    });
+    test('Test permitPriority priority passed', async () => {
+        let authService = Container.get(AuthService);
+        let ctx = createMockContext();
+        ctx.request.headers.authorization = '123412341';
+        let next = jest.fn(async()=>{});
+        authService.permitPriority([1])(ctx, next);
+        expect(ctx.state.access).toBeDefined();
         expect(ctx.state.access).toBeTruthy();
-    })
-    test('Priority 6 can not access', () => {
-        const verify = jwt.verify as jest.MockedFunction<(st: string)=> Record<string, unknown> | string>;
-        verify.mockReturnValue({priority: 6, id: 22});
-        ctx.request.headers.authorization = '12341234123';
-        permit([1,2,3,4,5])(ctx, async()=>{});
-        expect(ctx.state.access).toBeFalsy();
-    })
-});
-
-describe('Testing authenticate function works', () => {
-    test('userRepo findOne return {user: `mock`}', async () => {
-        const user = await userRepo().findOne();
-        expect(user).toMatchObject({ 
-            _id: '33',
-            id: '22',
-            email: 'gg',
-            password: '**'
-        });
-    })
-})
-describe('Authenticate ', () => {
-    test('Hit the ctx.assert ', async () => {
-        let ctx = createMockContext();
-        const assert = jest.spyOn(ctx, 'assert');
-        await authenticate(ctx, async()=>{});
-        expect(assert).toHaveBeenCalled();
-    })
-    test('Hit userRepo.findOne', async () => {
-        let ctx = createMockContext();
-        const findOne = jest.spyOn(userRepo(), 'findOne');
-        await authenticate(ctx, async()=>{});
-        expect(findOne).toHaveBeenCalled();
-    })
-    test('Check ctx.state.user value correct', async () => {
-        let ctx = createMockContext();
-        ctx.request.headers.authorization = 'anything'
-        await authenticate(ctx, async ()=>{});
-        expect(ctx.state.user).toMatchObject({ 
-            _id: '33',
-            id: '22',
-            email: 'gg',
-            password: '**'
-        });
     })
 })
